fc6cfd409c35bca1c1c9f7ce612ca91c7ce9d992
DamianQuezada <damianquezada.s@gmail.com>
2024-11-01 20:18:19 -0400

    Fixed a minor bug in dark-theme

--- src/app/shared/components/theme-toggle/theme-toggle.component.ts
+++ src/app/shared/components/theme-toggle/theme-toggle.component.ts
@@ -8,9 +8,9 @@ import { Component, effect, OnInit, signal } from '@angular/core';
   templateUrl: './theme-toggle.component.html',
   styleUrl: './theme-toggle.component.css',
   host: {
-    class: 'flex flex-row w-[35%] p-[0.15rem] rounded-3xl border border-slate-300',
+    class: 'flex flex-row w-[35%] 2xl:w-[38%] p-[0.15rem] rounded-3xl border border-slate-300',
     '[class.bg-teal-100/70]': 'isDarkTheme()',
-    '[class.bg-neutral-100]': '!isDarkTheme()'
+    '[class.bg-neutral-200]': '!isDarkTheme()'
   }
 })
 export class ThemeToggleComponent {
@@ -22,18 +22,13 @@ export class ThemeToggleComponent {
   constructor() {
     // Update the class on the document element when theme changes
     effect(() => {
-      if (this.isDarkTheme()) {
-        document.documentElement.classList.add('dark');
-        localStorage.setItem('theme','dark');
-      } else {
-        document.documentElement.classList.remove('dark');
-        localStorage.setItem('theme','light');
-      }
+      document.documentElement.classList.toggle('dark', this.isDarkTheme());
+      document.documentElement.classList.toggle('bg-slate-800', this.isDarkTheme());
+      localStorage.setItem('theme', this.isDarkTheme() ? 'dark' : 'light');
     });
   }
 
   toggleTheme() {
     this.isDarkTheme.update(value => !value);
   }
-
 }
